9e1ae22517c3c874ed04d2869a9d07894631e6d7
Dennis Mbithi <denmbithi@gmail.com>
2023-09-08 11:12:41 +0300

    Added blog model and admin views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "status"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    """Model defining blog post"""
+
+    STATUSCHOICE = ((1, "Draft"), (2, "Published"))
+
+    title = models.CharField(max_length=200)
+    post = models.TextField()
+    status = models.CharField(max_length=100, choices=STATUSCHOICE, default=1)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="blog")
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Post"
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Category(models.Model):
+    """Model defination for Categories."""
+
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return self.name
