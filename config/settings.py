fa68c9d2971649516e89073d34174b72197586e2
dennis mbithi <denmbithi@gmail.com>
2023-09-10 14:09:58 +0000

    Updated setting for deployment

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import socket
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -7,10 +8,32 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0peo@#x9jur3!h$ryje!$879xww8y1y66jx!%*#ymhg&jkozs2"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
-DEBUG = True
 
+if socket.gethostname() == 'djblog':
+    DEBUG = True
+    PUBLIC_DIR = ‘/var/www/djblog/public/’
+    EMAIL_BACKEND = ‘django.core.mail.backends.smtp.EmailBackend’
+
+    DATABASES = {
+        'default': {
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
+            'NAME': 'djblog',
+            'USER': 'postgres',
+            'PASSWORD': 'friction',
+            'HOST': 'localhost',
+            'PORT': '5432',
+            },
+        }
+else:
+    DEBUG = True
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "10.29.135.213"]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = [
@@ -70,12 +93,7 @@ TEMPLATES = [
 ]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
 
 # For Docker/PostgreSQL usage uncomment this and comment the DATABASES config above
 # DATABASES = {
